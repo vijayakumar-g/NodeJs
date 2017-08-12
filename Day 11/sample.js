86e47398b47419f4e43e9e5cd403623ae58c0179
vijayakumar-g <vkgvijay4@gmail.com>
2017-08-12 14:45:40 +0530

    ok

---
+++ Day 11/sample.js
@@ -0,0 +1,134 @@
+var sprintf=require("sprintf");
+var fs=require("fs");
+function calendar()
+{
+//week array has the day with the corresponding index value
+ var week={
+  0:"sun",
+  1:"mon",
+  2:"tue",
+  3:"wed",
+  4:"thu",
+  5:"fri",
+  6:"sat"
+}
+// month array has the no of days for each month that has
+var month={
+  1:31,
+  2:28,
+  3:30,
+  4:31,
+  5:30,
+  6:31,
+  7:31,
+  8:31,
+  9:30,
+  10:31,
+  11:30,
+  12:31
+}
+//month array with respective month details
+var monthExp={
+  1:"jan",
+  2:"feb",
+  3:"mar",
+  4:"apr",
+  5:"may",
+  6:"jun",
+  7:"jul",
+  8:"aug",
+  9:"sept",
+  10:"oct",
+  11:"nov",
+  12:"dec"
+}
+
+var m=document.getElementById("month").value; //reading the month from the user
+var y=document.getElementById("year").value; //reading the year from the user
+var y0 = Math.floor(y+4800-((14-m)/12));
+var x = Math.floor(y0+y0/4-y0/100 + y0/400);
+var m0 = Math.floor(m + 12 * ((14 - m) / 12)-3);
+//calculating the day of which the month is going to be starts
+var day=Math.floor((2+((153 *(m0)+ 2) / 5)+ (365 * x)- 32045));
+day=day%7;
+//for the array no of week of the month week is taken in an account
+var dd=month[m];
+var w=dd%7;
+var weeks=w+2;
+//creating a array with the 2d to display the calendar
+var c=0;
+var start=1;
+document.write("javascript calender:"+m+" "+y);
+document.write("<br>"+monthExp[m]+" "+y);
+document.write("<br>sun    mon      tue       wed       thur    fri      sat <br>");
+function Node(d) {
+  this.obj= d;
+  this.next = null;
+}
+function queue()
+{
+  this.rear =0;
+  this.front=0;
+}
+function week_withdays(d,da)
+{
+  this.day=d;
+  this.date=da;
+}
+queue.prototype.display = function() //display the elements in the node
+{
+  currentNode = this.rear;
+  if (currentNode == null) //checking for empty node
+  {
+  document.write("Node is Empty");
+   }
+  if(currentNode.next!=null&& currentNode==this.front) //checking for node to be not null
+   {
+    console.log(currentNode.obj.day);
+     //traversing till the last
+    currentNode = currentNode.next;
+
+  }
+}
+queue.prototype.enqueue=function(d)
+{
+  var node=new Node(d);
+  if(this.rear==null)
+  {
+    this.rear=node;
+    this.front=this.rear;
+  }
+  else {
+      this.rear.next=node;
+      node.obj=d;
+      this.rear=node;
+      }
+  document.write(this.rear.obj.date+"\t");
+
+}
+var Week=new queue();
+for(var i=0; i<=weeks; i++)
+{
+
+  for(var j=0; j<7; j++)
+  {
+    if(j<day) //to print the space value here my space value= 0 is taken to display in proper manner
+    {
+      var weekday=new week_withdays(week[j],"0");
+
+    }
+    else {
+      if((start<=month[m]+1))
+      {
+        var weekday=new week_withdays(week[j],start);
+        start++;
+
+      }
+    }
+      Week.enqueue(weekday);
+    }
+  document.write("<br>");
+  day=0;
+}
+}
+//Week.display();
---
+++ Day 11/weekqueueStack.js
@@ -0,0 +1,14 @@
+function Node(d,da) {
+  this.date= d;
+  this.day=da;
+  this.next = null;
+}
+function week(Node)
+{
+  this.days=Node;
+}
+function queue()
+{
+  this.rear =0;
+  this.front=0;
+}
